eb206fe4aef0f5bf8d034dbee989e9a080197292
Faheem Ahmad <faheem.ahmad@pb.com>
2021-11-11 17:58:21 +0530

    Delete release.config.js

--- release.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  branches: ["mster"],
-  repositoryUrl: "https://github.com/faheempb/react-app",
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/github"
-  ]
-};
