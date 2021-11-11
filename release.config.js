f7cb868cbb45500608181536722ff6525e41ac92
Faheem Ahmad <35453270+faheemjmi@users.noreply.github.com>
2021-11-11 15:20:22 +0530

    feat: new feature1

--- release.config.js
+++ release.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-    branches: "mster",
-    repositoryUrl: "https://github.com/faheempb/react-app",
-    plugins: [
-        "@semantic-release/commit-analyzer", 
-        "@semantic-release/release-notes-generator", 
-        "@semantic-release/npm",
-        "@semantic-release/github"
-    ]
-
-}
+  branches: "mster",
+  repositoryUrl: "https://github.com/faheempb/react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+  ],
+};
