a2c18a073826ef29f023da93d8d3f35a1f8db46d
Viplove Vashishtha <viplove.vashishtha@pb.com>
2021-11-12 19:24:25 +0530

    fix: changed the name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React by PB
+          Learn React by Pitney Bowes
         </a>
       </header>
     </div>
