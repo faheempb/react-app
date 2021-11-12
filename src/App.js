fd6518e1ba3799c1de4db4430ee0a9b01ba6eacf
Viplove Vashishtha <viplove.vashishtha@pb.com>
2021-11-12 18:54:39 +0530

    change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React by Faheem
+          Learn React by PB
         </a>
       </header>
     </div>
